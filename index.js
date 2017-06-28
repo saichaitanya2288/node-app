781b996bf525be5e5567c31311d42c03417665e7
devops <devops@gmail.com>
2017-06-28 18:40:00 +0530

    added 4

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello NODE3!!!!')
+  response.send('Hello NODE4!!!!')
 })
 
 app.listen(app.get('port'), function() {
