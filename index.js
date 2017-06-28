bd9e4b448b9a2b9cd7a7cbe380c4ec4a0181c181
devops <devops@gmail.com>
2017-06-28 16:52:35 +0530

    added digi

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Lync!!!!')
+  response.send('Hello Digi!!!!')
 })
 
 app.listen(app.get('port'), function() {
