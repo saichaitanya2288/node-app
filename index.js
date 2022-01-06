520f27549695ad4460b220776ecae82b7413fe77
Mahendra8899 <90463908+Mahendra8899@users.noreply.github.com>
2022-01-06 16:16:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Welcome !!!!')
+  response.send('Hello c360softe !!!!')
 })
 
 app.listen(app.get('port'), function() {
