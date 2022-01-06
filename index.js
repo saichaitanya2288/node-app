08a95c35ce8f82965c52195a3e00ac5d290a78a5
Mahendra8899 <90463908+Mahendra8899@users.noreply.github.com>
2022-01-06 15:44:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Vijay!!!!')
+  response.send('Hello Mahendra Vardhan!!!!')
 })
 
 app.listen(app.get('port'), function() {
