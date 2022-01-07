726fca6b6a47715a75cfdee86102c91b448e8490
Sai Chaitanya Achyuta <91956973+saichaitanya2288@users.noreply.github.com>
2022-01-07 12:29:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello c360softe !!!!')
+  response.send('Hello c360soft chaitanya !!!!')
 })
 
 app.listen(app.get('port'), function() {
